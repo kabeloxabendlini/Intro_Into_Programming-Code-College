28dd54ebf75b22411ec6d5706969f58a594f4fb6
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-13 15:27:29 +0200

    Update JavaScript.

--- My_Code/Chapter 3/main.js
+++ My_Code/Chapter 3/main.js
@@ -11,3 +11,12 @@ var namesOfDifferentTypeOfPeople = [
 ];
 
 namesOfDifferentTypeOfPeople[0] = "Me";
+namesOfDifferentTypeOfPeople[1] = "Dad";
+namesOfDifferentTypeOfPeople[2] = "Sister";
+namesOfDifferentTypeOfPeople[3] = "Best Friend (Bantu)";
+namesOfDifferentTypeOfPeople[4] = "Best Friend (Samkelo)";
+namesOfDifferentTypeOfPeople[5] = "Best Friend (Werner)";
+namesOfDifferentTypeOfPeople[6] = "Best Friend (Rafe)";
+namesOfDifferentTypeOfPeople[7] = "Best Friends (Axel)";
+namesOfDifferentTypeOfPeople[8] = "Mother";
+
