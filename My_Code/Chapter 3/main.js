f929676f7e1212eb9aca886895d862d71e6421b1
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-13 16:36:37 +0200

    Update main.js

--- My_Code/Chapter 3/main.js
+++ My_Code/Chapter 3/main.js
@@ -21,3 +21,7 @@ namesOfDifferentTypeOfPeople[7] = "Best Friends (Axel)";
 namesOfDifferentTypeOfPeople[8] = "Mother";
 
 var random = [1, 2, "string", "string2", ["Kabelo",[12500 , 13500, 14500], "Kabelo"], true]
+
+var lottery = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
+
+lottery[Math.floor(Math.floor() * 8)]
