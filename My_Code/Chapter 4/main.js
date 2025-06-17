bafed8a77989efb3f0fa1c7614c7e739a7abb8b1
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-17 16:11:51 +0200

    Update JavaScript.

--- My_Code/Chapter 4/main.js
+++ My_Code/Chapter 4/main.js
@@ -6,8 +6,29 @@ var myPerson = {
     "Province": "KwaZulu - Natal",
 };
 
-myPerson["myFavouriteHobbies"] = ["Exercising", "Computing", "Reading", "Meditating", "Thinking", 
-        "Socializing"];
+myPerson["myFavouriteHobbies"] = ["Exercising", "Computing", "Reading", "Meditating", "Thinking",
+    "Socializing"];
 
-    
 
+var myFamily = {
+    "Tamiya Xabendlini": {
+        dateOfBirth: 21 / 12 / 1999,
+        gender: Female,
+        Hobbies: ["Culinary", "NetFlix", "Exercise"],
+        Age: "25"
+    },
+
+    "uTa Xabendlini": {
+        dateOfBirth: 29 / 9 / 1973,
+        gender: Male,
+        Hobbies: ["Carpentry", "Exercising", "Documentaries", "Meditating in prayer"],
+        Age: "51"
+    },
+
+    "uMa Xabendlini": {
+        dateOfBirth: 4 / 7 / 1975,
+        gender: Female,
+        Hobbies: ["Teaching", "Jogging", "Entertainment"],
+        Age: "49"
+    }
+};
