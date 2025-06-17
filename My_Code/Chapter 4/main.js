701a1892ccd60d63c914414f94b771eefffac85c
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-17 15:24:35 +0200

    Update Main.JS

--- My_Code/Chapter 4/main.js
+++ My_Code/Chapter 4/main.js
@@ -1,5 +1,12 @@
 var myPerson = {
     "myAge": 19,
     "eyeColour": "brown",
-    "race": "black"
+    "race": "black",
+    "nationality": "South Africa",
+    "Province": "KwaZulu - Natal",
 };
+
+myPerson["myFavouriteHobbies"] = ["Exercising", "Computing", "Reading", "Meditating", "Thinking", 
+        "Socializing"
+    ]
+
