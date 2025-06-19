9f1b518a3dff3c1e056170bce0999db87cf172a5
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-19 14:53:33 +0200

    Update JavaScript.

--- My_Code/Chapter 6,program/main.js
+++ My_Code/Chapter 6,program/main.js
@@ -23,9 +23,17 @@
 
 // ***************************Line 21 to 27*************************************
 
-var myAge = 0;
+// var myAge = 0;
 
-while (random < 10) {
-    console.log(random)
-    random++
+// while (random < 10) {
+//     console.log(random)
+//     random++
+// };
+
+// ***************************Line 34 to 39*************************************
+
+for (var myAge = 0; myAge >= 19; myAge <= 19){
+    console.log("Kabelo is myAge; " + myAge)
 };
+
+alert("You are done.")
