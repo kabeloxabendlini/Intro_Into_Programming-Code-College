c7d70a3c94d8526829973b1367ede9b210f80697
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-19 14:05:37 +0200

    Update JavaScript.

---
+++ My_Code/Chapter 6,program/main.js
@@ -0,0 +1,12 @@
+var myName = prompt("Please set your name?").toTitleCase();
+
+if (myName == "Kabelo Xabendlini") {
+    console.log("hello" + myName);
+} else if (myName == ""){
+    console.log("hello" + myName)
+
+} else {
+
+};
+
+console.log("Goodbye");
