4a0b73c90e57369d15c30fb8f0c7c9ed97817fcf
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-19 14:40:57 +0200

    Update JavaScript.

--- My_Code/Chapter 6,program/main.js
+++ My_Code/Chapter 6,program/main.js
@@ -1,18 +1,31 @@
-var myName = prompt("Please set your name?").toLowerCase();
+// ***************************Line 1 to 14*************************************
 
-if (myName == "kabelo xabendlini") {
-    console.log("hello" + myName);
-} else if (myName == "tamiya xabendlini"){
-    console.log("hello" + myName)
+// var myName = prompt("Please set your name?").toLowerCase();
 
-} else {
+// if (myName == "kabelo xabendlini") {
+//     console.log("hello" + myName);
+// } else if (myName == "tamiya xabendlini"){
+//     console.log("hello" + myName)
 
+// } else {
+
+// };
+
+// console.log("Goodbye");
+
+// ***************************Line 16 to 20*************************************
+
+// var myAge= prompt("What is your age?");
+
+// while (myAge == 19) {
+//     console.log("You are 19.")
+// }
+
+// ***************************Line 21 to 27*************************************
+
+var myAge = 0;
+
+while (random < 10) {
+    console.log(random)
+    random++
 };
-
-console.log("Goodbye");
-
-var myAge= prompt("What is your age?");
-
-while (myAge == 19) {
-    console.log("You are 19.")
-}
