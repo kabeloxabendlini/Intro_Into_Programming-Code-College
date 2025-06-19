0ac78882ad4daa541f055be8e245a3ad7b549888
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-19 14:36:39 +0200

    Update Javascript.

--- My_Code/Chapter 6,program/main.js
+++ My_Code/Chapter 6,program/main.js
@@ -6,7 +6,13 @@ if (myName == "kabelo xabendlini") {
     console.log("hello" + myName)
 
 } else {
-    
+
 };
 
 console.log("Goodbye");
+
+var myAge= prompt("What is your age?");
+
+while (myAge == 19) {
+    console.log("You are 19.")
+}
