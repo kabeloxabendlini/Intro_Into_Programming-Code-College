861af5bf0c55027c8637189f357cd0a9699ad8f2
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-20 14:04:03 +0200

    Update challenege.html

---
+++ My_Code/Chapter 6,program/challenge.js
@@ -0,0 +1,8 @@
+var namesOfPeopleIKnow = ["Kabelo", "Tamiya", "Werner", "Samkelo", "Diego", 
+    "uTa Xabendlini", "uMa Xabendlini"];
+   
+for (var i = 0; i < namesOfPeopleIKnow.length; i++) {
+    namesOfPeopleIKnow[i] = " Honourable and prestiges " + namesOfPeopleIKnow[i]
+};
+
+console.log(namesOfPeopleIKnow);
