126559b5a402f69ed6f857be534e7a2c1ecc82ac
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-20 16:09:23 +0200

    Hangman Game.

---
+++ My_Code/Chapter 7/hangman.js
@@ -0,0 +1,43 @@
+var words = [
+    "javascript",
+    "monkey",
+    "amazing",
+    "pancake"
+];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+var remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+    // Game code goes here
+    // Show the player their progress
+    alert(answerArray, join(""));
+
+    // Take input from the player
+    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+    if (guess === null) {
+        break;
+
+    } else if (guess.length !== 1) {
+        alert("Please enter a single letter.");
+    } else {
+        // Update the game state with the guess
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
+    }
+
+    // Update answerArray and remainingLetters for every correct guess
+    alert(answerArray.join(" "));
+    alert("Good job! The answer was " + word);
+
+}
+
