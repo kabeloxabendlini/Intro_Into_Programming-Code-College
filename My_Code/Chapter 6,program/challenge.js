c3caca6003cc0b8e50e5a79fb0109601151039d1
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-20 15:24:41 +0200

    Update JavaScript.

--- My_Code/Chapter 6,program/challenge.js
+++ My_Code/Chapter 6,program/challenge.js
@@ -25,13 +25,32 @@
 
 // ******************************************** Line 26 - 34 *****************************************
 
-var name = prompt("Please set your name?");
+// var name = prompt("Please set your name?");
 
-for (var i = 0; i < name.length; i++) 
-  if (name.length > 7) {
- console.log("Wow, you have a REALLY long name!");
-} else {
- console.log("Your name isn't very long.");
-};
+// for (var i = 0; i < name.length; i++) 
+//   if (name.length > 7) {
+//  console.log("Wow, you have a REALLY long name!");
+// } else {
+//  console.log("Your name isn't very long.");
+// };
 
-// ******************************************** Line 26 - 34 *****************************************
+// ******************************************** Line 39 - 54 *****************************************
+
+var input = "javascript is awesome";
+var output = " ";
+
+for (var i = 0; i < input.length; i++) {
+    if (input[0] == "a") {
+        output += "4"
+    } else if (input[i] == "0") {
+        output += "3"
+    } else if (input[i] == "i") {
+        output += "i"
+    } else if (input[i] == "o") {
+        output += "0"
+    } else {
+        out
+    }
+}
+
+// ******************************************** Line 39 - 54 *****************************************
