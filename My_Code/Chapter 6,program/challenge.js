f82f7df601b773f893edebe46fdc704330396f9f
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-20 14:16:20 +0200

    Update Challenge.

--- My_Code/Chapter 6,program/challenge.js
+++ My_Code/Chapter 6,program/challenge.js
@@ -1,8 +1,24 @@
-var namesOfPeopleIKnow = ["Kabelo", "Tamiya", "Werner", "Samkelo", "Diego", 
-    "uTa Xabendlini", "uMa Xabendlini"];
+// var namesOfPeopleIKnow = ["Kabelo", "Tamiya", "Werner", "Samkelo", "Diego", 
+//     "uTa Xabendlini", "uMa Xabendlini"];
    
-for (var i = 0; i < namesOfPeopleIKnow.length; i++) {
-    namesOfPeopleIKnow[i] = " Honourable and prestiges " + namesOfPeopleIKnow[i]
+// for (var i = 0; i < namesOfPeopleIKnow.length; i++) {
+//     namesOfPeopleIKnow[i] = " Honourable and prestiges " + namesOfPeopleIKnow[i]
+// };
+
+// console.log(namesOfPeopleIKnow);
+
+// ******************************************** Line 12 - 16 *****************************************
+
+// var animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+
+// for (var i = 0; i < animals.length; i++) {
+//  console.log("This zoo contains a " + animals[i] + ".");
+// }
+
+// ******************************************** Line 20 - 24 *****************************************
+
+var numbers = "float('inf')";{
+var randomString = alphabet[Math.floor(Math.random) * numbers.length]   
 };
 
-console.log(namesOfPeopleIKnow);
+console.log(randomString);
