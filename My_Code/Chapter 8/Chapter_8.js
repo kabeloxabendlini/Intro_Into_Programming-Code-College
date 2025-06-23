5df828694630062c39da5b9f655ee4b713405f6e
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-23 16:06:10 +0200

    Update Chapter_8.js

--- My_Code/Chapter 8/Chapter_8.js
+++ My_Code/Chapter 8/Chapter_8.js
@@ -1,10 +1,10 @@
-function printMyName(firstName, surname) {
-    alert("Hello." + firstName + " " + surname)
-};
+// function printMyName(firstName, surname) {
+//     alert("Hello." + firstName + " " + surname)
+// };
 
-printMyName(prompt("What is your first name"), prompt("What is your surname ?"));
+// printMyName(prompt("What is your first name"), prompt("What is your surname ?"));
 
-// **************************** Line 9 - 17 *********************************************** 
+// **************************** Line 9 - 17 ***********************************************// 
 
 // function printOut() {
 //     alert("My Name Is Kabelo Xabendlini.")
@@ -15,3 +15,11 @@ printMyName(prompt("What is your first name"), prompt("What is your surname ?"))
 // greetings.toUpperCase()
 
 // console.log()
+
+// **************************** Line 19 - 25 ************************************************//
+
+function printThis() {
+    for (var i = 0; i < firstParse; i++) {
+        console.log("You printed" + secondParse)
+    }
+};
