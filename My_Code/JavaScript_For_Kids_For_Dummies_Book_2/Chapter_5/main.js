a051677a0e61100b3f3e7330896a64685d0f8fa4
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-24 14:33:00 +0200

    Update JavaScript.

---
+++ My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_5/main.js
@@ -0,0 +1,31 @@
+var item1;
+var item2;
+var item3;
+var item4;
+var item5;
+
+document.getElementById("changeList").onclick = newList;
+document.getElementById("myName").style.color = "red";
+document.getElementsByClassName()
+
+function newList() {
+    item1 = prompt("Enter a new first thing: ");
+    item2 = prompt("Enter a new second thing: ");
+    item3 = prompt("Enter a new third thing: ");
+    item4 = prompt("Enter a new fourth thing: ");
+    item5 = prompt("Enter a new fifth thing: ");
+    updateList();
+}
+
+function updateList() {
+    document.getElementById("firstThing").innerHTML =
+        item1;
+    document.getElementById("secondThing").innerHTML =
+        item2;
+    document.getElementById("thirdThing").innerHTML =
+        item3;
+    document.getElementById("fourthThing").innerHTML =
+        item4;
+    document.getElementById("fifthThing").innerHTML =
+        item5;
+}
