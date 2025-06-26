b55b77b6441212793b7dc40602bd51c8d8339fef
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-26 16:08:16 +0200

    Update JavaScript.

--- My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_7/robots.js
+++ My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_7/robots.js
@@ -13,38 +13,38 @@ document.getElementById("body").style.color = "#FF0000";
 // Give Douglas hair.
 document.getElementById("head").style.borderTop = "6px black solid";
 
-// var rightEye = document.getElementById("righteye");
-// rightEye.addEventListener("click", moveUpDown);
+var rightEye = document.getElementById("righteye");
+rightEye.addEventListener("click", moveUpDown);
 
-// function moveUpDown(e) {
-//     var robotPart = e.target;
-//     var top = 0;
-//     var id = setInterval(frame, 10) // draw every 10ms
+function moveUpDown(e) {
+    var robotPart = e.target;
+    var top = 0;
+    var id = setInterval(frame, 10) // draw every 10ms
 
-//     function frame() {
-//         robotPart.style.top = top + '%';
-//         top++;
-//         if (top === 20) {
-//             clearInterval(id);
-//         }
-//     }
-// }
+    function frame() {
+        robotPart.style.top = top + '%';
+        top++;
+        if (top === 20) {
+            clearInterval(id);
+        }
+    }
+}
 
-// var leftArm = document.getElementById("leftarm");
-// leftArm.addEventListener("click", moveRightLeft);
+var leftArm = document.getElementById("leftarm");
+leftArm.addEventListener("click", moveRightLeft);
 
-// function moveRightLeft(e) {
-//     var robotPart = e.target;
-//     var left = 0;
-//     var id = setInterval(frame, 10) // draw every 10ms
-//     function frame() {
-//         robotPart.style.left = left + '%';
-//         left++;
-//         if (left === 70) {
-//             clearInterval(id);
-//         }
-//     }
-// }
+function moveRightLeft(e) {
+    var robotPart = e.target;
+    var left = 0;
+    var id = setInterval(frame, 10) // draw every 10ms
+    function frame() {
+        robotPart.style.left = left + '%';
+        left++;
+        if (left === 70) {
+            clearInterval(id);
+        }
+    }
+}
 
 var greenColor = "green";
 
