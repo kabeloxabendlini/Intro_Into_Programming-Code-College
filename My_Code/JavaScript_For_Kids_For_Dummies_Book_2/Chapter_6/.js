04df1db521f79b7ed895b16e0cc9d9f7e13d8381
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-26 15:35:01 +0200

    Update JavaScript.

---
+++ My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_6/.js
@@ -0,0 +1,31 @@
+document.getElementById("lefteye").style.backgroundColor = "purple";
+document.getElementById("head").style.transform = "rotate(15deg)";
+// Put a 2‐pixel‐wide, solid black border around his body.
+document.getElementById("body").style.border = "2px blacksolid";
+// Round the corners of his mouth.
+document.getElementById("mouth").style.borderRadius = "4px";
+// Put yellow dots around his right eye.
+document.getElementById("righteye").style.border = "4px yellow dotted";
+// Change his left arm’s color.
+document.getElementById("leftarm").style.backgroundColor = "#FF00FF";
+// Change the text color.
+document.getElementById("body").style.color = "#FF0000";
+// Give Douglas hair.
+document.getElementById("head").style.borderTop = "6px black solid";
+
+// var rightEye = document.getElementById("righteye");
+// rightEye.addEventListener("click", moveUpDown);
+
+// function moveUpDown(e) {
+//     var robotPart = e.target;
+//     var top = 0;
+//     var id = setInterval(frame, 10) // draw every 10ms
+
+//     function frame() {
+//         robotPart.style.top = top + '%';
+//         top++;
+//         if (top === 20) {
+//             clearInterval(id);
+//         }
+//     }
+// }
