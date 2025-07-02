dcbdb75056a0a8b38029305132626914a02e7056
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-07-02 15:42:16 +0200

    Update chap12.js

--- My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_12/chap12.js
+++ My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_12/chap12.js
@@ -1,44 +1,44 @@
-// var trainSpeed = 250;
-// var trainPosition = 0;
-// var animation;
+var trainSpeed = 250;
+var trainPosition = 0;
+var animation;
 
-// var train = document.getElementById("train");
-// train.addEventListener("click", speedUp);
+var train = document.getElementById("train");
+train.addEventListener("click", speedUp);
 
-// var stopButton = document.getElementById("stopButton");
-// stopButton.addEventListener("click", stopTrain);
+var stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", stopTrain);
 
-// function speedUp() {
-//     if (trainSpeed > 10) {
-//         trainSpeed -= 10;
-//     }
-//     console.log("train speed: " + trainSpeed);
+function speedUp() {
+    if (trainSpeed > 10) {
+        trainSpeed -= 10;
+    }
+    console.log("train speed: " + trainSpeed);
 
-//     clearInterval(animation);
-//     animation = setInterval(frame, trainSpeed);
+    clearInterval(animation);
+    animation = setInterval(frame, trainSpeed);
 
 
-//     function frame() {
-//         trainPosition += 2;
-//         train.style.left = trainPosition + 'px';
-//         console.log(trainPosition);
-//         checkPosition(trainPosition);
-//     }
-// }
+    function frame() {
+        trainPosition += 2;
+        train.style.left = trainPosition + 'px';
+        console.log(trainPosition);
+        checkPosition(trainPosition);
+    }
+}
 
-// function checkPosition(currentPosition) {
-//     if (currentPosition === 420) {
-//         alert("OOOOO!");
-//         console.log("Crash!");
-//         clearInterval(animation);
-//     }
-// }
+function checkPosition(currentPosition) {
+    if (currentPosition === 420) {
+        alert("OOOOO!");
+        console.log("Crash!");
+        clearInterval(animation);
+    }
+}
 
-// function stopTrain() {
-//     if (trainPosition < 420) {
-//         clearInterval(animation);
-//     }
-// }
+function stopTrain() {
+    if (trainPosition < 420) {
+        clearInterval(animation);
+    }
+}
 
 function stopTrain(trainPosition) {
     if (trainPosition === 420) {
