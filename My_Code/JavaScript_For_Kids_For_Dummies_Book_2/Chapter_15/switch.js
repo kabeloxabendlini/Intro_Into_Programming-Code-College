e2349fc828aa28ff5c58a964638ff0de2e2cab43
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-07-07 16:47:50 +0200

    Update Repository.

--- My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_15/switch.js
+++ My_Code/JavaScript_For_Kids_For_Dummies_Book_2/Chapter_15/switch.js
@@ -2,62 +2,66 @@
 
 // ************************************ Line 1 - 31 ********************************************//
 
-// var myNumber = prompt("Please can you name the favourite day of the week !").toUpperCase;
+var myNumber = prompt("Please can you name the favourite day of the week !").toUpperCase();
 
-// var theResponse;
+var theResponse;
 
-// switch (myNumber) {
-//     case "MONDAY":
-//         theResponse = "Ack!";
-//         break;
-//     case "TUESDAY":
-//         theResponse = "Taco day!";
-//         break;
-//     case "WEDNESDAY":
-//         theResponse = "Halfway there!";
-//         break;
-//     case "THURSDAY":
-//         theResponse = "It's the new Friday!";
-//         break;
-//     case "FRIDAY":
-//         theResponse = "TGIF! Yeah!";
-//         break;
-//     case "SATURDAY":
-//         theResponse = "What a day!";
-//         break;
-//     case "SUNDAY":
-//         theResponse = "Sunday = Funday!";
-//         break;
-//     default:
-//         theResponse = "I haven't heard of that one!";
-//         break;
-// }
+switch (myNumber) {
+    case "MONDAY":
+        theResponse = "Ack!";
+        break;
+    case "TUESDAY":
+        theResponse = "Taco day!";
+        break;
+    case "WEDNESDAY":
+        theResponse = "Halfway there!";
+        break;
+    case "THURSDAY":
+        theResponse = "It's the new Friday!";
+        break;
+    case "FRIDAY":
+        theResponse = "TGIF! Yeah!";
+        break;
+    case "SATURDAY":
+        theResponse = "What a day!";
+        break;
+    case "SUNDAY":
+        theResponse = "Sunday = Funday!";
+        break;
+    default:
+        theResponse = "I haven't heard of that one!";
+        break;
+}
+
+
+alert(theResponse);
 
 // ************************************ Line 1 - 31 *********************************************//
 
 // ************************************ Line 36 - 56 ********************************************//
 
-var myNumber = prompt("Please can you name the favourite day of the week !").toUpperCase()
+// var myNumber = prompt("Please can you name the favourite day of the week !").toUpperCase()
 
-var theResponse;
+// var theResponse;
 
-if (myNumber === "MONDAY") {
-    theResponse = "Ack!";
-} else if (myNumber === "TUESDAY") {
-    theResponse = "Taco day!";
-} else if (myNumber === "WEDNESDAY") {
-    theResponse = "Halfway there!";
-} else if (myNumber === "THURSDAY") {
-    theResponse = "It's the new Friday!";
-} else if (myNumber === "FRIDAY") {
-    theResponse = "TGIF! Yeah!";
-} else if (myNumber === "SATURDAY") {
-    theResponse = "What a day!";
-} else if (myNumber === "SUNDAY") {
-    theResponse = "Sunday = Funday!";
-} else {
-    theResponse = "I haven't heard of that one!";
-}
+// if (myNumber === "MONDAY") {
+//     theResponse = "Ack!";
+// } else if (myNumber === "TUESDAY") {
+//     theResponse = "Taco day!";
+// } else if (myNumber === "WEDNESDAY") {
+//     theResponse = "Halfway there!";
+// } else if (myNumber === "THURSDAY") {
+//     theResponse = "It's the new Friday!";
+// } else if (myNumber === "FRIDAY") {
+//     theResponse = "TGIF! Yeah!";
+// } else if (myNumber === "SATURDAY") {
+//     theResponse = "What a day!";
+// } else if (myNumber === "SUNDAY") {
+//     theResponse = "Sunday = Funday!";
+// } else {
+//     theResponse = "I haven't heard of that one!";
+// }
+// ************************************ ALERT RESPONSE PROMPT ************************************//
 
 alert(theResponse);
 
